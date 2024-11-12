fb99d22a224167a8abd4fb02b05f7f9d489f1486
OzzyD1 <ozzydomarkas@gmail.com>
2024-11-12 20:12:05 +0000

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
 package ie.setu
 
 fun main() {
-    println("Hello World!")
+
+}
+
+fun mainMenu(): Int {
+    print(""" 
+        > ----------------------------------
+         > |        SIM RACE TRACKER APP    |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Add a note                |
+         > |   2) List all notes            |
+         > |   3) Update a note             |
+         > |   4) Delete a note             |
+         > |   5) Archive a note            |
+         > |   6) Search note (by desc)     |
+         > ----------------------------------
+         > |   20) Save notes               |
+         > |   21) Load notes               |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ---------------------------------- 
+         >""".trimMargin(">"))
+    return readNextInt(" > ==>>")
 }
