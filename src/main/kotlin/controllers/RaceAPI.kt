0220433206eed3d416bde56ee318b72faac2202f
OzzyD1 <ozzydomarkas@gmail.com>
2024-11-23 13:27:36 +0000

    Added code addRace() but it is still not functional

---
+++ src/main/kotlin/controllers/RaceAPI.kt
@@ -0,0 +1,13 @@
+package ie.setu.controllers
+
+import ie.setu.models.Race
+
+class RaceAPI {
+
+    private var races = ArrayList<Race>()
+
+    fun add(note: Race): Boolean{
+        return races.add(race)
+    }
+
+}
