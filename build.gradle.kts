500c03c4232e486cefc35248f385309f5813fa58
OzzyD1 <ozzydomarkas@gmail.com>
2024-11-25 17:05:56 +0000

    Added XMLSerializer

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,10 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging:7.0.0")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
     implementation("org.slf4j:slf4j-simple:2.0.16")
-
+    // https://mvnrepository.com/artifact/com.thoughtworks.xstream/xstream
+    implementation("com.thoughtworks.xstream:xstream:1.4.21")
+    // https://mvnrepository.com/artifact/org.codehaus.jettison/jettison
+    implementation("org.codehaus.jettison:jettison:1.5.4")
 }
 
 tasks.test {
