597e3d6465ea8ef4d217fc5c7b50d29a25f5ddae
OzzyD1 <ozzydomarkas@gmail.com>
2024-11-26 14:13:35 +0000

    Added KDoc documentation for RaceAPI.kt

--- src/main/kotlin/models/Lap.kt
+++ src/main/kotlin/models/Lap.kt
@@ -1,13 +1,11 @@
 package ie.setu.models
 
-//Duration object is very useful
 //https://kotlinlang.org/docs/time-measurement.html#get-string-representation
 import kotlin.time.Duration
 
 data class Lap (
 //Starting lap at 1 because it makes sense in the context but will this have coding consequences?
     var lapId: Int = 1,
-//TODO: Write function to convert to time format or rethink the entire strategy
     var lapTime: Duration,
     var pitTime: String,
     var yellowFlag: String,
